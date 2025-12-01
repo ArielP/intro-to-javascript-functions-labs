c8a41f1340c6c127de7acca7db458e7c71a18022
ArielP <peguero.ariel@gmail.com>
2025-12-01 17:41:31 -0500

    homework done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-console.log('working')
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+function maxOfTwoNumbers (num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2
+  }
+}
+
+console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+
+
 /*
 Exercise 2: isAdult()
 
@@ -9,18 +28,17 @@ Example: isAdult(21) should return 'Adult'.
 
 Complete the exercise in the space below:
 */
-function isAudit(x){
-    return "Adult"
-}
-    else {
+function isAudit(age){
+    if (age >= 18) {
+      return 'Adult' ;
+    } else {
+      return 'Minor' :
+    }
 
-}
-     return "Minor";
 
-    console.log (isAdult(x))
+console.log('Exercise 2 Result:', isAdult(21));
 
-    
-} /*
+ /*
 Exercise 3: isCharAVowel()
 
 Write a function named isCharAVowel that takes a single character as 
@@ -32,20 +50,13 @@ Example: isCharAVowel('a') should return true.
 
 Complete the exercise in the space below:
 */
-function isCharAVowel(x){
-    if (x == "a"|| x ==="e"){
-        return true
-    }
-     else {
-        return false
-     }
-
+function isCharAVowel(char){
+    const vowels = 'aeiouAEIOU';
+    return vowels.includes(char);
+    
 }
-// Test the function
-console.log ('Exercise 3 Result:', isCharAVowel("b")); 
+console.log('Exercise 3 Result:', isCharAVowel("a"));
 
-
-console.log(isCharAVowel("a"));
 
 /*
 Exercise 4: generateEmail()
@@ -58,8 +69,8 @@ should return 'johnsmith@example.com'.
 
 Complete the exercise in the space below:
 */
-function generateEmail(x){
-
+function generateEmail(name, domain) {
+    return name + '@' + domain; 
 }
  return '${name}@$domain}';
 
@@ -78,14 +89,13 @@ Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
 
 Complete the exercise in the space below:
 */
-function greetUser("Sam", "morning"){
-
+function greetUser(name, timeOfDay){}
+    return 'Good ${timeOfDay}, ${name}!';
 }
-
-return ("Good morning, Sam!")
 
 
 console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
+
 
 /*
 Exercise 6: maxOfThree()
@@ -97,16 +107,85 @@ Example: maxOfThree(17, 4, 9) should return 17.
 
 Complete the exercise in the space below:
 */
-
-function maxOfThree(17,4,9){
-    
-    return 17
+function maxOfThree(num1, num2, num3) {
+    return Math.max(num1, num2, num3);
 }
 
 console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
 
 
+/*
+Exercise 7: calculateTip()
 
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+function calculateTip(billAmount, tipPercentage){
+    return billAmount * (tipPercentage / 100)
+
+}
+
+
+console.log('Exercise 7 Result:', calculateTip(50, 20));
+
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+function convertTemperature (temp, scale) {
+    if (scale === 'C') {
+        return temp * 9/5 + 32;
+    }   else if (scale === 'F') {
+        return (temp - 32 * 5/9);
+    }   
+}
+
+
+console.log('Exercise 8 Result:', convertTemperature(32, "C"));
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+function basicCalculator(num1, num2, operation) {
+    if (operation === 'add') {
+        return num1 + num2;
+    }   else if (operation === 'subtract') {
+        return num1 - num2
+    }   else if (operation === 'multiply') {
+        return num1 * num2;
+    }   else if (operation === 'divide')   {
+        return num1 / num2;
+    }
+}
+
+
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
 
 
 
